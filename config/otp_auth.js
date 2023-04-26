0176813bc23cbf0b524b6c85bfc025a92d6824f8
SOJAN <66105552+sojanonelson@users.noreply.github.com>
2023-04-26 23:02:24 +0530

    Update otp_auth.js

--- config/otp_auth.js
+++ config/otp_auth.js
@@ -1,5 +1,5 @@
 module.exports = {
   serviceSID: "VAf29c7ad4d8fb60b868f0d2910e878165",
   accoutnSID: "AC9e8272e0fb19439dd78c5096f6f3f70a",
-  authToken: "182c4915a05ab2aadb4930889f537918",
+  authToken: "c474bb335e1ac14a716408ed7f15a3a5",
 };
