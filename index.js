cb427700744d75d95a5ed5425e7e5ca90c0885d7
SOJAN <66105552+sojanonelson@users.noreply.github.com>
2023-04-26 22:47:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const testRouter = require("./Router/testRouter")
+const testRouter = require("./Router/testRouter");
+const router = require("express").Router();
 
 const app = express();
 
