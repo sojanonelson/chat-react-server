7957579713c95d70edf8ff0429235b163670bcf1
SOJAN <66105552+sojanonelson@users.noreply.github.com>
2023-04-26 22:46:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ app.use(
 app.use(bodyParser.json());
 app.use("/", testRouter)
 
+router.post("/mobile", (req, res) => {
+  console.log("number", req.body.number);
+  client.verify
+    .services(serviceSID)
+    .verifications.create({
+      to: `+91${req.body.number}`,
+      channel: "sms",
+    })
+    .then((resp) => {
+      console.log("response", resp);
+      res.status(200).json(resp);
+    });
+});
+
 const PORT = "8000";
 app.listen(PORT, () => {
   console.log(`Port running on ${PORT} `);
