856de45904b62171ae7596350d0988c79146af64
SOJAN <66105552+sojanonelson@users.noreply.github.com>
2023-04-29 13:05:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ const { Configuration, OpenAIApi } = require("openai");
 const app = express();
 
 
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+
 app.use(bodyParser.json());
 app.use("/", testRouter)
 
@@ -26,14 +20,15 @@ const configuration = new Configuration({
   apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
-app.post("/chat", async (req, res) => {
+app.post("/chat", async (req, res, next) => {
   console.log("openai response gets successfully")
   const { prompt } = req.body;
   const completion = await openai.createCompletion({
     model: "text-davinci-003", 
     prompt: prompt,
     max_tokens: 500,
-  }).catch(error => { throw error});
+  })catch (err) {
+    next(err);
   res.send(completion.data.choices[0].text);
 });
 
